7ba5e497de5ee011874c42fc5d93dd0df850fea9
Ecapolicchio <75374141+Ecapolicchio@users.noreply.github.com>
2021-06-20 14:11:56 -0300

    tpe.sh modified
    Al menos las 2 lineas curl y las 2 lineas java creo que deben estar bien.
    DETALLE:
    solo andaría en Linux, pues en Windows para llamar una variable es '%VARIABLE%'

--- Tpe.sh
+++
@@ -1,3 +0,0 @@
-curl  http://api.sportradar.us/nascar-ot3/mc/2019/drivers/list.xml?api_key=2wqh3eaxgxpcfyfvesqjft3b -o drivers_list.xml
-
-curl  http://api.sportradar.us/nascar-ot3/mc/2019/standings/drivers.xml?api_key=2wqh3eaxgxpcfyfvesqjft3b -o drivers_standings.xml
---
+++ tpe.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+valid_race_types=("sc" "xf" "cw" "go" "mc" "enas")
+valid_dates=("2013" "2021")
+
+if [ "$#" -eq "2" ]; then 
+	if [ "$1" -ge "${valid_dates[0]}" ];
+	then 
+		if [ "$1" -le "${valid_dates[1]}" ];
+		then 
+			for i in "$valid_race_types";
+			do
+				if ["$2" == "$i"];
+				then
+					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
+
+					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/standings/drivers.xml?api_key=${SPORTRADAR_API} -o drivers_standings.xml
+
+					java net.sf.saxon.Query extract_nascar_data.xq !indent=yes -o:nascar_data.xml
+
+					java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_markdown.xsl [ -o:nascar_page.md ]
+
+				else
+
+				fi
+				echo "Parameter 2: 'race_type' is incorrect"
+			done
+		else
+			"Parameter 1: 'year' must be lesser than or equal to '2021' "
+		fi
+	else
+		echo "Parameter 1: 'year' must be greater than or equal to '2013' "
+	fi
+else
+	echo "There is needed 2 parameters: 'year' 'race_type' "
+fi
