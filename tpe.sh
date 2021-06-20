21a9e2f2e052f622d1b209390a080158d432e9b0
aBenve <abenvenuto@itba.edu.ar>
2021-06-20 19:16:12 -0300

    agregue un flag para cuando el tipo de carrera es invalido

--- tpe.sh
+++ tpe.sh
@@ -3,6 +3,8 @@
 valid_race_types=("sc" "xf" "cw" "go" "mc" "enas")
 valid_dates=("2013" "2021")
 
+founded="1"
+
 if [ "$#" -eq "2" ]; then
 	if [ "$1" -ge "${valid_dates[0]}" ];
 	then
@@ -14,21 +16,24 @@ if [ "$#" -eq "2" ]; then
 				then
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
 
-					java net.sf.saxon.Transform -s:drivers_list.xml -xsl:remove_xmlns.xsl  [ -o:drivers_list.xml ]
+					java net.sf.saxon.Transform -s:drivers_list.xml -xsl:remove_xmlns.xsl  -o:drivers_list.xml
 
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/standings/drivers.xml?api_key=${SPORTRADAR_API} -o drivers_standings.xml
 
-					java net.sf.saxon.Transform -s:drivers_standings.xml -xsl:remove_xmlns.xsl [ -o:drivers_standings.xml ]
+					java net.sf.saxon.Transform -s:drivers_standings.xml -xsl:remove_xmlns.xsl -o:drivers_standings.xml
 
 					java net.sf.saxon.Query extract_nascar_data.xq !indent=yes -o:nascar_data.xml
 
-					java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_markdown.xsl [ -o:nascar_page.md ]
-
-				else
+					java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_markdown.xsl -o:nascar_page.md
+				
+					founded="0"
 
 				fi
-				echo "Parameter 2: 'race_type' is incorrect"
 			done
+			if [ "$founded" == "1" ];
+			then
+		   		echo "Parameter 2: 'race_type' is incorrect"
+		   	fi
 		else
 			"Parameter 1: 'year' must be lesser than or equal to '2021' "
 		fi
