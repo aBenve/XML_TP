4b43620a2bfce1386b207067a3ae3cdc20f8c9c0
aBenve <abenvenuto@itba.edu.ar>
2021-06-21 16:12:43 -0300

    corregi error de echo, sigue sin andar

--- tpe.sh
+++ tpe.sh
@@ -15,21 +15,13 @@ if [ "$#" -eq "2" ]; then
 				if [ "$2" == "$i" ];
 				then
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
-
 					java net.sf.saxon.Transform -s:drivers_list.xml -xsl:remove_xmlns.xsl  -o:drivers_list.xml
-
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/standings/drivers.xml?api_key=${SPORTRADAR_API} -o drivers_standings.xml
-
 					java net.sf.saxon.Transform -s:drivers_standings.xml -xsl:remove_xmlns.xsl -o:drivers_standings.xml
-
 					java net.sf.saxon.Query extract_nascar_data.xq !indent=yes -o:nascar_data.xml
-
 					java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_markdown.xsl -o:nascar_page.md
-
 					java net.sf.saxon.Transform -s:nascar_data.xml -xsl:generate_markdown_html.xsl -o:nascar_page_html.html
-				
 					founded="0"
-
 				fi
 			done
 
@@ -38,7 +30,7 @@ if [ "$#" -eq "2" ]; then
 		   		echo "Parameter 2: 'race_type' is incorrect"
 		   	fi
 		else
-			"Parameter 1: 'year' must be lesser than or equal to '2021' "
+			echo "Parameter 1: 'year' must be lesser than or equal to '2021' "
 		fi
 	else
 		echo "Parameter 1: 'year' must be greater than or equal to '2013' "
