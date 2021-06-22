831cd09a288604fc2e0af499df311a8ea1a7201b
Ecapolicchio <75374141+Ecapolicchio@users.noreply.github.com>
2021-06-22 00:24:53 -0300

    'enas' solo puede ser en '2020'

--- tpe.sh
+++ tpe.sh
@@ -3,6 +3,8 @@
 valid_race_types=("sc" "xf" "cw" "go" "mc" "enas")
 valid_dates=("2013" "2021")
 
+special_race_date=("enas" "2020")
+
 founded="1"
 
 if [ "$#" -eq "2" ]; then
@@ -11,10 +13,18 @@ if [ "$#" -eq "2" ]; then
 		if [ "$1" -le "${valid_dates[1]}" ];
 		then
 			for i in "${valid_race_types[@]}";
-			do
+			do			
 				if [ "$2" == "$i" ];
 				then
-
+					# Si estamos hablando de una carrera que solo se celebró un determinado año
+					
+					if [ "$2" == "${special_race_date[0]}" ];
+					then
+						if [ "$1" -ne "${special_race_date[1]}" ];
+						then ./get_error.sh "Parameter 1: Parameter 2 is ok but ' "${special_race_date[0]}" ' its only supported for ' "${special_race_date[1]}" ' season"
+						fi
+					fi
+					
 					# Si los argumentos son validos, se almacena la informacion en nascar_data.xml
 
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
