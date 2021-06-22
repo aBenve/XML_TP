b75ff25e8211dec77108e19a3757161cda484f6c
abril <aocchipinti@itba.edu.ar>
2021-06-22 16:55:03 -0300

    update get_error.sh

--- get_error.sh
+++ get_error.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-echo "<nascar_data><error>$1</error></nascar_data>" >  nascar_data.xml
+echo '<?xml version="1.0" encoding="UTF-8"?>' > nascar_data.xml
+echo '<nascar_data xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="nascar_data.xsd">' >> nascar_data.xml
+echo '<error>' $1 '</error>' >>  nascar_data.xml
+echo '</nascar_data>' >>  nascar_data.xml
--- tpe.sh
+++ tpe.sh
@@ -13,11 +13,11 @@ if [ "$#" -eq "2" ]; then
 		if [ "$1" -le "${valid_dates[1]}" ];
 		then
 			for i in "${valid_race_types[@]}";
-			do			
+			do
 				if [ "$2" == "$i" ];
 				then
 					# Si estamos hablando de una carrera que solo se celebró un determinado año
-					
+
 					if [ "$2" == "${special_race_date[0]}" ];
 					then
 						if [ "$1" -ne "${special_race_date[1]}" ];
@@ -28,15 +28,15 @@ if [ "$#" -eq "2" ]; then
 						exit 1
 						fi
 					fi
-					
+
 					# Si los argumentos son validos, se almacena la informacion en nascar_data.xml
 
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
 					java net.sf.saxon.Transform -s:drivers_list.xml -xsl:remove_xmlns.xsl  -o:drivers_list.xml
-					
+
 					curl http://api.sportradar.us/nascar-ot3/"$2"/"$1"/standings/drivers.xml?api_key=${SPORTRADAR_API} -o drivers_standings.xml
 					java net.sf.saxon.Transform -s:drivers_standings.xml -xsl:remove_xmlns.xsl -o:drivers_standings.xml
-					
+
 					java net.sf.saxon.Query extract_nascar_data.xq !indent=yes -o:nascar_data.xml
 					founded="0"
 				fi
@@ -52,7 +52,7 @@ if [ "$#" -eq "2" ]; then
 		./get_error.sh "Parameter 1: 'year' must be greater than or equal to '2013' "
 	fi
 else
-	./get_error.sh "There is needed 2 parameters: 'year' 'race_type' "
+	./get_error.sh "There are needed 2 parameters: 'year' 'race_type' "
 fi
 
 # Sea que llego con error o no, se ejecuta y se obtiene un markdown y pagina HTML
